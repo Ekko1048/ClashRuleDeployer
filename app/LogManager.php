e08979b99610d577a5c1383bd69c457b84fe2e2c
Ekko <158275702+Ekko1048@users.noreply.github.com>
2025-05-27 16:51:10 +0800

    Update LogManager.php

--- app/LogManager.php
+++ app/LogManager.php
@@ -1,1 +1,24 @@
+<?php
+// app/LogManager.php
+class LogManager {
+    private $file;
 
+    public function __construct(string $file) {
+        $this->file = $file;
+        if (!file_exists($file)) {
+            file_put_contents($file, '');
+        }
+    }
+
+    public function append(string $text) {
+        file_put_contents($this->file, date('[Y-m-d H:i:s] ') . $text . "\n", FILE_APPEND);
+    }
+
+    public function read(): string {
+        return file_get_contents($this->file);
+    }
+
+    public function clear() {
+        file_put_contents($this->file, '');
+    }
+}
